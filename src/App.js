4de6a56d081b16d7fe7e6aa7568a3b19875368e0
Your Name <you@example.com>
2022-06-02 23:32:29 +0530

    add search and select filed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
-const App = () =>{
-  return(
+const App = () => {
+  return (
     <div>
-      <h1>Pet </h1>
-      <Pet name = "Luna" animal="dog" breed="havanese"/>
-      <Pet name = "B" animal="Bird" breed="cock"/>
-      <Pet name = "Luna" animal="dog" breed="havanese"/>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
     </div>
   );
 };
 
-render(<App/>,document.getElementById("root")) ;
-
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-//   ]);
-// };
-
-// render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
